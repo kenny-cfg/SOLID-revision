871666082be5457a7a6ad393490a5cbfbb0f24f8
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-18 18:53:37 +0000

    Very solid SRP

---
+++ solid/single_responsibility/file_manager.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+class FileManager:
+    def __init__(self, filename):
+        self.path = Path(filename)
+
+    def read(self, encoding="utf-8"):
+        return self.path.read_text(encoding)
+
+    def write(self, data, encoding="utf-8"):
+        self.path.write_text(data, encoding)
--- solid/single_responsibility/zip_file_manager.py
+++ solid/single_responsibility/zip_file_manager.py
@@ -1,15 +1,6 @@
 from pathlib import Path
 from zipfile import ZipFile
 
-class FileManager:
-    def __init__(self, filename):
-        self.path = Path(filename)
-
-    def read(self, encoding="utf-8"):
-        return self.path.read_text(encoding)
-
-    def write(self, data, encoding="utf-8"):
-        self.path.write_text(data, encoding)
 
 class ZipFileManager:
     def __init__(self, filename):
